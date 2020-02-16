24e1a21030f3d59f105ca0a376ac22ef6acc124b
gmanriquev <gmanrique.422@gmail.com>
2020-02-16 13:37:43 -0500

    Archivos semana 06

--- SEM-06/Parte03.sql
+++ SEM-06/Parte03.sql
@@ -16,3 +16,36 @@ from Ficha f
 join Manzana m on f.idmanzana=m.idmanzana
 
 SELECT * FROM F_RMANZANA()
+
+--6.4
+CREATE VIEW V_REPORTE_E
+AS
+select 
+f.idficha,
+f.montopago,
+CONCAT(p.nombres,' ' ,p.apellidos) as NOMENC,
+COUNT(1) OVER(PARTITION BY e.idencuestador) as E_NUM_FICHAS,
+SUM(montopago) OVER(PARTITION BY e.idencuestador) as E_TOT_MTOPAGO,
+MAX(montopago) OVER(PARTITION BY e.idencuestador) as E_MAX_MTOPAGO,
+MIN(montopago) OVER(PARTITION BY e.idencuestador) as E_MIN_MTOPAGO,
+AVG(montopago) OVER(PARTITION BY e.idencuestador) as E_PROM_MTOPAGO
+from Ficha f
+join Encuestador e on f.idencuestador=e.idencuestador
+join Persona p on e.idpersona=p.idpersona
+
+select * from V_REPORTE_E
+
+--6.5
+select 
+m.nombre as MANZANA,
+f.idficha as IDFICHA,
+f.numhabitantes as NUMHABITANTES,
+ROW_NUMBER() OVER(PARTITION BY m.nombre ORDER BY f.numhabitantes ASC) as RN,--POSICIONES IRREPETIBLES
+RANK() OVER(PARTITION BY m.nombre ORDER BY f.numhabitantes ASC) as RK,--POSICIONES REPETIBLES CON SALTOS DE POSICIÓN
+DENSE_RANK() OVER(PARTITION BY m.nombre ORDER BY f.numhabitantes ASC) as DRK,--POSICIONES REPETIBLES SIN SALTOS DE POSICIÓN
+NTILE(4) OVER(PARTITION BY m.nombre ORDER BY f.numhabitantes ASC) as NTILE4--DENTRO DE CADA MANZANA DIVIDIR EN 4 GRUPOS
+from Ficha f
+join Manzana m on f.idmanzana=m.idmanzana
+order by m.nombre,f.numhabitantes ASC
+
+
